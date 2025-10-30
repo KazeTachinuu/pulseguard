6bda5ab512d3d9b73c6e2ff3afc14b65c3eb6443
robin.roger <robin.roger@epita.fr>
2025-10-30 12:31:36 +0100

    (fix): failed pipeline

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,33 +3,43 @@
 Ceci sera tster
 - Saisie du master pass
 - creation & ajout
-- Déverrouillage 
+- Déverrouillage
 - test cancel avec ctrl-D ou ctrl-C
 """
 
-import builtins
-import pytest
 from unittest.mock import patch
-from pulseguard.auth import get_master_password, prompt_create_master_password, prompt_unlock_vault
+
+import pytest
+
+from pulseguard.auth import (
+    get_master_password,
+    prompt_create_master_password,
+    prompt_unlock_vault,
+)
+
 
 @patch("getpass.getpass", side_effect=["Secret!"])
 def test_get_master_password_basic(mock_gp):
     assert get_master_password() == "Secret!"
 
+
 @patch("getpass.getpass", side_effect=["Pass1", "Pass1"])
 def test_prompt_create_master_password_ok(mock_gp):
     assert prompt_create_master_password() == "Pass1"
+
 
 @patch("getpass.getpass", side_effect=["Pass1", "Other"])
 def test_prompt_create_master_password_mismatch(mock_gp):
     with pytest.raises(ValueError):
         prompt_create_master_password()
 
+
 @patch("getpass.getpass", side_effect=KeyboardInterrupt)
 def test_get_master_password_cancelled(mock_gp, capsys):
     with pytest.raises(KeyboardInterrupt):
         get_master_password()
     assert "Password prompt cancelled" in capsys.readouterr().err
+
 
 @patch("getpass.getpass", return_value="OpenSesame")
 def test_prompt_unlock_vault(mock_gp):
